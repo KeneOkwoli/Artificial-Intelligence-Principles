3d8d1ee97cce253a12fe9b11a0be7cd9be41989a
Kene Okwoli <72444503+KeneOkwoli@users.noreply.github.com>
2024-12-03 16:39:19 -0500

    Checked if player has connected 4 then wins

--- Connect4.py
+++ Connect4.py
@@ -9,11 +9,11 @@ COLUMNS_NUMBER = 7
 
 
 def DrawBoard():
-    board = numpy.zeros((6,7))
+    board = numpy.zeros((ROWS_NUMBER,COLUMNS_NUMBER))
     return board
 
 def DropToken(board, row, column, token):
-    board[row][column] == token
+    board[row][column] = token
 
 
 def LocationValid(board, column):
@@ -23,9 +23,38 @@ def NextRow(board,column):
     for r in range(ROWS_NUMBER):
         if board[r][column] == 0:
             return r
+        
+def Win(board, token):
+#checks horizontal for any wins
+    for c in range(COLUMNS_NUMBER - 3):
+        for r in range(ROWS_NUMBER):
+            if board[r][c] == token and board[r][c+1] == token and board[r][c+2] == token and board[r][c+3] == token:
+                return True
+#checks horizontal for any wins
+    for c in range(COLUMNS_NUMBER):
+        for r in range(ROWS_NUMBER - 3):
+            if board[r][c] == token and board[r+1][c] == token and board[r+2][c] == token and board[r+3][c] == token:
+                return True               
+            
+#checking diaginals to the right
+    for c in range(COLUMNS_NUMBER - 3):
+        for r in range(ROWS_NUMBER - 3 ):
+            if board[r][c] == token and board[r+1][c+1] == token and board[r+2][c+2] == token and board[r+3][c+3] == token:
+                return True
+
+
+#checking diagonals to the left
+    for c in range(COLUMNS_NUMBER - 3):
+        for r in range(3, ROWS_NUMBER - 3 ):
+            if board[r][c] == token and board[r-1][c+1] == token and board[r-2][c+2] == token and board[r-3][c+3] == token:
+                return True            
+
+def FlipBoard(board):
+    print(numpy.flip(board,0))
+
 
 board = DrawBoard()
-print(board)
+FlipBoard(board)
 turn = 0
 gameOver = False
 
@@ -37,14 +66,23 @@ while not gameOver:
         if LocationValid(board, column):
             row = NextRow(board, column)
             DropToken(board, row, column, 1)
+            if Win(board,1):
+                print("Player 1 is the winner!")
+                gameOver = True
+                break
     
     # Player 2 input
     else: 
         column = int(input("Player 2s choice: "))
         if LocationValid(board, column):
             row = NextRow(board, column)
-            DropToken(board, row, column, 2)        
+            DropToken(board, row, column, 2)       
+            if Win(board,2):
+                print("Player 2 is the winner!")
+                gameOver = True
+                break             
 
+    FlipBoard(board)
     turn +=1
     turn = turn % 2
 
