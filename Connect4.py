4aa576b197d5134599f824b73454b512d796e47f
Kene Okwoli <72444503+KeneOkwoli@users.noreply.github.com>
2024-12-03 14:39:05 -0500

    Created the board for the game in the terminal

---
+++ Connect4.py
@@ -0,0 +1,14 @@
+import numpy
+
+def DrawBoard():
+    board = numpy.zeros((6,7))
+    return board
+
+board = DrawBoard()
+GameOver = False
+turn = 0
+
+while not GameOver:
+    # Player 1 input
+
+    # Player 2 input
