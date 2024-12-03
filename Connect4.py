92a0f5f1003967c59c5673f0a6fccf56a93de479
Kene Okwoli <72444503+KeneOkwoli@users.noreply.github.com>
2024-12-03 15:40:56 -0500

    New functions created

--- Connect4.py
+++ Connect4.py
@@ -1,14 +1,50 @@
+import random
+import pygame
+import sys
+import math
 import numpy
+
+ROWS_NUMBER = 6
+COLUMNS_NUMBER = 7
+
 
 def DrawBoard():
     board = numpy.zeros((6,7))
     return board
 
+def DropToken(board, row, column, token):
+    board[row][column] == token
+
+
+def LocationValid(board, column):
+    return board[ROWS_NUMBER - 1][column] == 0
+
+def NextRow(board,column):
+    for r in range(ROWS_NUMBER):
+        if board[r][column] == 0:
+            return r
+
 board = DrawBoard()
-GameOver = False
+print(board)
 turn = 0
+gameOver = False
 
-while not GameOver:
+
+while not gameOver:
     # Player 1 input
-
+    if turn == 0:
+        column = int(input("Player 1s choice: "))
+        if LocationValid(board, column):
+            row = NextRow(board, column)
+            DropToken(board, row, column, 1)
+    
     # Player 2 input
+    else: 
+        column = int(input("Player 2s choice: "))
+        if LocationValid(board, column):
+            row = NextRow(board, column)
+            DropToken(board, row, column, 2)        
+
+    turn +=1
+    turn = turn % 2
+
