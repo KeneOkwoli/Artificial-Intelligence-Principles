c73a31b15d72ffcfc22ae6738538abe3639233bd
Kene Okwoli <72444503+KeneOkwoli@users.noreply.github.com>
2024-12-04 23:14:34 +0000

    Fixed the board and the tokens work

--- Connect4.py
+++ Connect4.py
@@ -8,6 +8,8 @@ ROWS_NUMBER = 6
 COLUMNS_NUMBER = 7
 Black = (0,0,0)
 Blue = (0,0,255)
+Yellow = (255,255,0)
+Red = (255,0,0)
 
 
 def DrawBoard():
@@ -47,7 +49,7 @@ def Win(board, token):
 
 #checking diagonals to the left
     for c in range(COLUMNS_NUMBER - 3):
-        for r in range(3, ROWS_NUMBER - 3 ):
+        for r in range(3, ROWS_NUMBER):
             if board[r][c] == token and board[r-1][c+1] == token and board[r-2][c+2] == token and board[r-3][c+3] == token:
                 return True            
 
@@ -66,15 +68,24 @@ pygame.init()
 square_size = 100
 Radius = int(square_size/2 -8)
 board_width = COLUMNS_NUMBER * square_size
-board_height = (ROWS_NUMBER + 1) * square_size
+board_height = ROWS_NUMBER * square_size
 size = (board_width,board_height)
 screen = pygame.display.set_mode(size)
+pygame.display.update()
 
 def visual_board(board):
     for i in range(COLUMNS_NUMBER):
-        for j in range(ROWS_NUMBER + 1):
-            pygame.draw.rect(screen, Blue , (i*square_size , j*square_size + square_size, square_size, square_size) )
+        for j in range(ROWS_NUMBER):
+            pygame.draw.rect(screen, Blue , (i*square_size , j*square_size + square_size, square_size, square_size))
             pygame.draw.circle(screen, Black, (int(i*square_size+square_size/2), int(j*square_size+square_size/2)),Radius)
+	
+    for i in range(COLUMNS_NUMBER):
+        for j in range(ROWS_NUMBER):
+            if board [j][i] == 1:
+                pygame.draw.circle(screen, Yellow, (int(i*square_size+square_size/2), board_height-int(j*square_size+square_size/2)),Radius)
+            if board [j][i] == 2:
+                pygame.draw.circle(screen, Red, (int(i*square_size+square_size/2), board_height-int(j*square_size+square_size/2)),Radius)                
+    pygame.display.update()
 
 visual_board(board)
 pygame.display.update()
@@ -83,7 +94,15 @@ while not gameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
+        
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, Black ,(0,0,board_width, square_size))
+            x_pos = event.pos[0]
+            if turn == 0:
+                pygame.draw.circle(screen,Yellow, (x_pos,int(square_size/2)) , Radius)
+            else:
+                pygame.draw.circle(screen,Red, (x_pos,int(square_size/2)) , Radius)
+        pygame.display.update()
         if event.type == pygame.MOUSEBUTTONDOWN:
             print(event.pos)
              # Player 1 input
@@ -97,7 +116,7 @@ while not gameOver:
                     if Win(board,1):
                         print("Player 1 is the winner!")
                         gameOver = True
-                        break
+                        
             
             # Player 2 input
             else: 
@@ -109,9 +128,13 @@ while not gameOver:
                     if Win(board,2):
                         print("Player 2 is the winner!")
                         gameOver = True
-                        break             
+                                    
 
             FlipBoard(board)
+            visual_board(board)
             turn +=1
             turn = turn % 2 
+
+            if gameOver:
+                pygame.time.wait(3000)
 
